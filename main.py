31f7ad2222b56a24d1d62d14b5f27ca0bb80f477
KimJeongHun <jeonhun6084@gmail.com>
2023-01-03 17:26:06 +0900

    설명추가 및 타입지정 추가

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ from datetime import datetime
 
 def movie_to_image(
     video_name: str, image_folder: str, DROP_COUNT_MAX: int, END_FRAME: int
-):
+) -> None:
+    """
+    영상을 이미지로 만들어줍니다.
+    Args:
+        video_name (str) : 비디오 경로를 지정합니다.
+        image_folder (str) : 비디오에서 변환된 이미지를 저장하는 폴더 경로를 지정합니다.
+        DROP_COUNT_MAX (int) : 동영상 앞에 버릴 부분을 지정합니다.
+        END_FRAME (int) : 동영상 끝에 버릴 부분을 지정합니다.
+    Return
+        None
+    """
     vidcap = cv2.VideoCapture(video_name)
     FRAME_MAX = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
     FILE_NAME_COUNT = 0
@@ -36,7 +46,14 @@ def movie_to_image(
         print("DROP_COUNT_MAX > END_FRAME")
 
 
-def makeGif(image_folder: str):
+def makeGif(image_folder: str) -> None:
+    """
+    지정된 이미지 폴더를 GIF로 만들어줍니다.
+    Args:
+        image_folder (str) : GIF로 만들어줄 이미지 폴더를 지정합니다.
+    Return
+        None
+    """
     path = ["{}/{}".format(image_folder, i) for i in os.listdir(image_folder)]
     path.sort()
     path = natsort.natsorted(path)
@@ -46,7 +63,14 @@ def makeGif(image_folder: str):
     imageio.mimsave("result/{}.gif".format(str_now), paths, fps=12)
 
 
-def connting_max_frame(video_name):
+def counting_max_frame(video_name: str) -> int:
+    """
+    비디오의 최대 프레임수를 계산합니다.
+    Args:
+        video_name (str) : 비디오의 경로를 지정합니다.
+    Return
+        FRAME_MAX (int) : 영상의 최대프레임 수
+    """
     vidcap = cv2.VideoCapture(video_name)
     FRAME_MAX = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
     return FRAME_MAX
@@ -56,8 +80,17 @@ def connting_max_frame(video_name):
 def filter(
     image_sample_show: bool = True,
     filter: bool = False,
-    image_folder="./assets/image",
-):
+    image_folder: str = "./assets/image",
+) -> None:
+    """
+    사진의 사이즈를 조절하고 필터를 합니다.
+    Args:
+        image_sample_show (bool) : 샘플유무를 지정합니다.
+        filter (bool) : 필터유무를 지정합니다.
+        image_folder (str) : gif로 만들 이미지의 폴더경로를 지정합니다.
+    Return
+        None
+    """
     if image_sample_show:
         image1 = Image.open("assets/image/0_frame.png")
 
@@ -78,20 +111,29 @@ def filter(
             image.save(pth)
 
 
-def reset():
-    shutil.rmtree("./assets/image")
-    os.mkdir("./assets/image")
+def reset(image_folder: str) -> None:
+    """
+    이미지 폴더를 삭제하고 다시 작성합니다.
+    Args:
+        image_folder (str) : 비워낼 이미지 폴더를 지정합니다.
+    Return
+        None
+    """
+    shutil.rmtree(image_folder)
+    os.mkdir(image_folder)
 
 
 def main():
+    # gif로 쓰일 이미지 폴더지정 및 비디오 설정
     image_folder = "./assets/image"
     video_name = "./assets/movie/Test01.mp4"
+
     # 프레임 시작 끝 조절
     START_FRAME = 10
-    END_FRAME = connting_max_frame(video_name)
+    END_FRAME = counting_max_frame(video_name)
 
     # 이전에 했던 작업 지우기
-    reset()
+    reset(image_folder)
 
     # 영상에서 사진 가져오기
     movie_to_image(video_name, image_folder, START_FRAME, END_FRAME)
